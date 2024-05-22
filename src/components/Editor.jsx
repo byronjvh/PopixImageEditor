e8eaf1995b495863020c5e4ca9b66c704ecd667b
BYRON VARGAS <byronj.v.h.123@gmail.com>
2024-05-21 22:02:25 -0600

    Fixing canvas height

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -27,10 +27,12 @@ export function Editor () {
 
   return (
     <>
-      <canvas
-        ref={canvasRef}
-        className={'rounded-md p-2 bg-cyan-100 border-2 border-cyan-500'}
-      />
+      <div className='max-h-[50svh] flex justify-center rounded-md p-2 bg-cyan-100 border-2 border-cyan-500'>
+        <canvas
+          ref={canvasRef}
+          className='max-h-full max-w-full'
+        />
+      </div>
       <Menu />
     </>
   )
